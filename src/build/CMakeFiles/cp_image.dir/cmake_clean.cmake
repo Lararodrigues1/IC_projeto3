2c4d2f1404f40a6b4ec0e25deeb0d9b975b368aa
goncalo <gonc.soares12@ua.pt>
2022-12-08 15:13:51 +0000

    aa

---
+++ src/build/CMakeFiles/cp_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/goncaloaguiar/Desktop/IC_rep/IC3/IC_projeto3/bin/cp_image"
+  "/home/goncaloaguiar/Desktop/IC_rep/IC3/IC_projeto3/bin/cp_image.pdb"
+  "CMakeFiles/cp_image.dir/cp_image.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cp_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
